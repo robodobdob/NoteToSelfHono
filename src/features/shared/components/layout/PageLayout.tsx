214b20a6c6570eb91389921d9b8f3f4c5b19fc58
two-thirty-seven <83845920+two-thirty-seven@users.noreply.github.com>
2026-03-19 08:27:17 +1300

    Refactor PageLayout structure and update styles for navigation

--- src/features/shared/components/layout/PageLayout.tsx
+++ src/features/shared/components/layout/PageLayout.tsx
@@ -7,28 +7,25 @@ import ChatModal from "../../../chat/components/ChatModal";
 function PageLayout(props: any){
     return (
         <BodyLayout>
-            <div class="page">
-                <main>
-                    <div class="top-row">
-                        <a href="/" class="btn-link"><h3>Note To Self</h3></a>
-                        <span class="controls">
-                            <Working Id="mainLayout_spinner"/>
-                            <button type="button" class="btn btn-warning" command="show-modal" commandfor="chatModal">
-                                <Icon name="message-circle" />
-                            </button>
-                            <button type="button" class="btn btn-warning" command="show-modal" commandfor="utilityModal" hx-get="/notes/edit/00000000-0000-0000-0000-000000000000" hx-target="#utilityModal_content">
-                                <Icon name="plus" />
-                            </button>
-                        </span>
-                    </div>
+            <main>
+                <nav>
+                    <a href="/" class="btn-link"><h3>Note To Self</h3></a>
+                    <span class="controls">
+                        <Working Id="mainLayout_spinner"/>
+                        <button type="button" class="btn btn-warning" command="show-modal" commandfor="chatModal">
+                            <Icon name="message-circle" />
+                        </button>
+                        <button type="button" class="btn btn-warning" command="show-modal" commandfor="utilityModal" hx-get="/notes/edit/00000000-0000-0000-0000-000000000000" hx-target="#utilityModal_content">
+                            <Icon name="plus" />
+                        </button>
+                    </span>
+                </nav>
 
-                    {props.children}
-                </main>
+                {props.children}
 
                 <UtilityModal/>
                 <ChatModal/>
-
-            </div>
+            </main>
 
             <form method="post" action="/auth/logout" id="logout-form">
                 <button type="submit" class="btn btn-link">Logout</button>
